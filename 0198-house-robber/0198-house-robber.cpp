efeb08388695fdd8bf4c225e4bcba8b1ae29c63e
Rajatpundir7 <rajatpundir744@gmail.com>
2026-03-31 23:44:47 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,9 +9,9 @@ public:
             return nums[0];
         }
         dp[0] = nums[0];
-        dp[1] = max(nums[0] , dp[1]);
+        dp[1] = max(nums[0] , nums[1]);
         for(int i = 2 ;i < n; i++){
-           dp[i] = max(dp[i-2]+nums[i] , nums[i-1]);
+           dp[i] = max(dp[i-2]+nums[i] , dp[i-1]);
         }
         return dp[n-1] ;
     }
