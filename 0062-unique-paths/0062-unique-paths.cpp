549512605cdfd5f39d6a88718d6b8ed715cf4256
Rajatpundir7 <rajatpundir744@gmail.com>
2026-04-03 14:23:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,13 +14,25 @@ public:
         a *= i;
      }
      return (int)(a/(b*c));*/
-
+// aproch fing  the combination      
+// 
+/*
      int total = (m - 1 + n -1);
      long long  ans  =1 ;
      int r = min(n-1, m -1 );
      for(int i = 1 ;i <= r ;i++){
         ans = ans * ( total - r + i)/ i;
      }
-     return (int)ans;
+     return (int)ans;*/
+
+     vector<vector<int>> dp(m, vector<int>(n,1));
+
+     for(int i = 1 ; i < m; i++){
+        for(int j = 1; j< n;j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+     }
+     return dp[m-1][n-1];
     }
+
 };
