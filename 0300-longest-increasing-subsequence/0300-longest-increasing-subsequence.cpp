8c893f1e06ed3e311f0367bd9a12cb351d6b299c
Rajatpundir7 <rajatpundir744@gmail.com>
2026-04-06 09:27:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+    }
+};class Solution {
+public:
+    void min(int i ,vector<int> &dp , vector<int> &nums){
+        for(int j = 0 ;j< i;j++){
+            if(nums[i]>nums[j])
+            {
+                dp[i] = max(dp[i],dp[j]+1);
+            }
+        }
+        }
+    int lengthOfLIS(vector<int>& nums) {
+        int n  = nums.size();
+       vector<int> dp(n,1);
+       int ans  =0 ;
+       for(int i =0 ;i < n; i++){
+       /*{
+        if(nums[i] < nums[i+1]){
+            dp[i+1] = dp[i]+1;
+        }
+        else{
+            dp[i+1] = max(dp[i+1] , dp[i]);
+        }*/
+        min(i,dp,nums);
+        ans = max(ans,dp[i]);
+
+       }
+       return ans;
+    }
+};
