bc2ebfb1efb0d8b93a1eb4ca117100c9f7a1633f
Rajatpundir7 <rajatpundir744@gmail.com>
2026-06-22 08:55:53 +0530

    Time: 4 ms (40.06%), Space: 11.9 MB (48.41%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>freq(26,0);
+        for(char ch :magazine){
+            freq[ch-'a']++;
+        }
+        for(char ch : ransomNote){
+            if(freq[ch-'a'] > 0){
+                freq[ch-'a']--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
